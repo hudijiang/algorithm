8e7f84ea547a2a9c7eca06a1907b5d7902a90a06
hudijiang <343010985@qq.com>
2019-11-26 22:55:36 +0800

    单向链表

--- base/singlyLinkedList.go
+++ base/singlyLinkedList.go
@@ -1,11 +1,11 @@
 package base
 
-
 //declare a Node struct 声明一个Node结构体
 type Node struct {
-	next *Node
+	next  *Node
 	value interface{}
 }
+
 //declare a LinkList struct 声明一个链表结构体
 type LinkList struct {
 	head *Node
@@ -14,16 +14,15 @@ type LinkList struct {
 
 //create a new Node element 创建一个新的节点并给定一个val
 func NewNode(val interface{}) *Node {
-	return &Node{nil,val}
+	return &Node{nil, val}
 }
 
-//get this next Node element 获得当前
+//get this next Node element 获得当前 元素的下一个元素
 func (node *Node) GetNext() *Node {
 	return node.next
 }
-//get this Node value
+
+//get this Node value 获得当前元素的值
 func (node *Node) GetVal() interface{} {
 	return node.value
 }
-
-
