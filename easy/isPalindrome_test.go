9b86fc993efb833902dffc1f4a422d4ec6c47f1e
hudijiang <343010985@qq.com>
2020-02-24 00:01:57 +0800

    否是回文数test

---
+++ easy/isPalindrome_test.go
@@ -0,0 +1,13 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	test := []int{121, -121, 10, 13121, 12321}
+	for _, n := range test {
+		fmt.Printf("%d is palindrome ? %v\n", n, isPalindrome(n))
+	}
+}
